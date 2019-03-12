e12ed6775db3217239c396737de44e8530b1e7e9
EdwardSCharlesworth <edwardscharlesworth@gmail.com>
2019-03-12 10:50:56 -0400

    add csv

--- lung_cancer_surv.py
+++ lung_cancer_surv.py
@@ -41,11 +41,13 @@ X = encode_categorical(X)
 n_censored = y.shape[0] - y["status"].sum()
 print("%.1f%% of records are censored" % (n_censored / y.shape[0] * 100))
 
-plt.figure(figsize=(9, 6))
-val, bins, patches = plt.hist((y["time"][y["status"]],
-                               y["time"][~y["status"]]),
-                              bins=30, stacked=True)
-plt.legend(patches, ["Time of Death", "Time of Censoring"])
+for sex in LUNG_CLM['ph.ecog'].unique():
+    msk = LUNG_CLM['sex'] ==sex    
+    plt.figure(figsize=(9, 6))
+    val, bins, patches = plt.hist((y["time"][msk][y["status"][msk]],
+                                   y["time"][msk][~y["status"][msk]]),
+                                  bins=30, stacked=True)
+    plt.legend(patches, ["Time of Death", "Time of Censoring"])
 #%%
 for group in LUNG_CLM['ph.ecog'].unique():
     mask = X["ph.ecog"] == group
@@ -56,23 +58,24 @@ for group in LUNG_CLM['ph.ecog'].unique():
     plt.step(time, surv_prob, where="post",
              label="Treatment = {}".format(group))
 #%%
-estimator = FastSurvivalSVM(optimizer="rbtree", max_iter=1000, tol=1e-3, random_state=0)
 
 def score_survival_model(model, X, y):
     prediction = model.predict(X)
     result = concordance_index_censored(y['status'], y['time'], prediction)
     return result[0]
-
-param_grid = {'alpha': 2. ** np.arange(-20, 20, 2)}
-cv = ShuffleSplit(n_splits=200, test_size=0.5, random_state=0)
-gcv = GridSearchCV(estimator, param_grid, scoring=score_survival_model,
-                   n_jobs=4, iid=False, refit=False,
-                   cv=cv)
-
-
-warnings.filterwarnings("ignore", category=UserWarning)
-gcv = gcv.fit(X, y)
-print(gcv.best_score_, gcv.best_params_)
+warnings.filterwarnings("once")
+optimizers=("avltree","direct-count","PRSVM","rbtree","simple")
+for m in optimizers:
+    estimator = FastSurvivalSVM(optimizer=m,random_state=1234)
+    param_grid = {'alpha': 2. ** np.arange(-13, 10, 2), 'tol': (.1, 1e-10), 'max_iter': (2, 200)}
+    cv = ShuffleSplit(n_splits=200, test_size=0.2, random_state=0)
+    gcv = GridSearchCV(estimator, param_grid, scoring=score_survival_model,
+                       n_jobs=4, iid=False, refit=False,
+                       cv=cv)
+    
+    gcv = gcv.fit(X, y)
+    print(m)
+    print(gcv.best_score_, gcv.best_params_)
 
 def plot_performance(gcv):
     n_splits = gcv.cv.n_splits
@@ -101,12 +104,11 @@ refit_gcv = GridSearchCV(estimator, param_grid, scoring=score_survival_model,
 refit_gcv = refit_gcv = refit_gcv.fit(X, y)
 gridsearch_rank = refit_gcv.predict(X)
 
-
 from sksurv.svm import FastKernelSurvivalSVM
 from sksurv.kernels import clinical_kernel
 
 kernel_matrix = clinical_kernel(X)
-kssvm = FastKernelSurvivalSVM(optimizer="rbtree", kernel="precomputed", random_state=0)
+kssvm = FastKernelSurvivalSVM(optimizer="rbtree", kernel="precomputed", random_state=1234)
 
 kgcv = GridSearchCV(kssvm, param_grid, score_survival_model,
                     n_jobs=4, iid=False, refit=False,
@@ -121,7 +123,7 @@ print(kgcv.best_score_, kgcv.best_params_)
 plot_performance(kgcv)
 
 refit_kgcv = GridSearchCV(kssvm, param_grid, score_survival_model,
-                          n_jobs=4, iid=False, refit=True,
+                          n_jobs=6, iid=False, refit=True,
                           cv=cv)
 refit_kgcv = refit_kgcv.fit(kernel_matrix, y)
 fast_kernel_SSVM_gcvrank = refit_kgcv.predict(kernel_matrix)
@@ -131,8 +133,12 @@ fast_kernel_SSVM_gcvrank = refit_kgcv.predict(kernel_matrix)
 X_train, X_test, y_train, y_test = train_test_split(X,
                                                    y,test_size=0.2,
                                                    random_state=4)
-estimator = CoxPHSurvivalAnalysis(alpha=9.5367431640625e-07)
+best_param=refit_kgcv.best_params_
+
+estimator = CoxPHSurvivalAnalysis(**refit_kgcv.best_params_)
 estimator.fit(X_train, y_train)
+estimator.score(X_test, y_test)
+
 
 pred_curves = estimator.predict_survival_function(X_test)
 for curve in pred_curves:
@@ -143,5 +149,5 @@ plt.clf()
 cum_curv = estimator.predict_cumulative_hazard_function(X_test)
 for item in cum_curv:
     plt.step(item.x, item.y, where="post")
-plt.clf()
+
 
