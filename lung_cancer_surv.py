a990b494e398e5d44ff3b36b6cb77c07b79d51d6
EdwardSCharlesworth <edwardscharlesworth@gmail.com>
2019-03-11 21:25:06 -0400

    cv work

--- lung_cancer_surv.py
+++ lung_cancer_surv.py
@@ -10,11 +10,13 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import GridSearchCV, KFold, ShuffleSplit
 from sksurv.column import encode_categorical
 from sksurv.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 from sksurv.linear_model import CoxnetSurvivalAnalysis, CoxPHSurvivalAnalysis
 from sksurv.nonparametric import kaplan_meier_estimator
 from sksurv.metrics import concordance_index_censored
 from sksurv.svm import FastSurvivalSVM
 import seaborn as sns
+import warnings
 
 LUNG_LOC = r"/home/ed/Desktop/lung.csv"
 LUNG_CLM = pd.read_csv(LUNG_LOC)
@@ -44,7 +46,7 @@ val, bins, patches = plt.hist((y["time"][y["status"]],
                                y["time"][~y["status"]]),
                               bins=30, stacked=True)
 plt.legend(patches, ["Time of Death", "Time of Censoring"])
-
+#%%
 for group in LUNG_CLM['ph.ecog'].unique():
     mask = X["ph.ecog"] == group
     time, surv_prob = kaplan_meier_estimator(
@@ -53,24 +55,24 @@ for group in LUNG_CLM['ph.ecog'].unique():
 
     plt.step(time, surv_prob, where="post",
              label="Treatment = {}".format(group))
-
-estimator = FastSurvivalSVM(optimizer="rbtree", max_iter=1000, tol=1e-6, random_state=0)
+#%%
+estimator = FastSurvivalSVM(optimizer="rbtree", max_iter=1000, tol=1e-3, random_state=0)
 
 def score_survival_model(model, X, y):
     prediction = model.predict(X)
     result = concordance_index_censored(y['status'], y['time'], prediction)
     return result[0]
 
-param_grid = {'alpha': 2. ** np.arange(-12, 13, 2)}
+param_grid = {'alpha': 2. ** np.arange(-20, 20, 2)}
 cv = ShuffleSplit(n_splits=200, test_size=0.5, random_state=0)
 gcv = GridSearchCV(estimator, param_grid, scoring=score_survival_model,
                    n_jobs=4, iid=False, refit=False,
                    cv=cv)
 
-import warnings
+
 warnings.filterwarnings("ignore", category=UserWarning)
 gcv = gcv.fit(X, y)
-gcv.best_score_, gcv.best_params_
+print(gcv.best_score_, gcv.best_params_)
 
 def plot_performance(gcv):
     n_splits = gcv.cv.n_splits
@@ -92,12 +94,12 @@ def plot_performance(gcv):
         t.set_rotation("vertical")
 
 plot_performance(gcv)
-
 refit_gcv = GridSearchCV(estimator, param_grid, scoring=score_survival_model,
-                   n_jobs=4, iid=False, refit=True,
-                   cv=cv)
+                         n_jobs=4, iid=False, refit=True,
+                         cv=cv)
 
-refit_gcv = gcv.fit(X, y)
+refit_gcv = refit_gcv = refit_gcv.fit(X, y)
+gridsearch_rank = refit_gcv.predict(X)
 
 
 from sksurv.svm import FastKernelSurvivalSVM
@@ -110,63 +112,36 @@ kgcv = GridSearchCV(kssvm, param_grid, score_survival_model,
                     n_jobs=4, iid=False, refit=False,
                     cv=cv)
 
-import warnings
+
 warnings.filterwarnings("ignore", category=UserWarning)
 kgcv = kgcv.fit(kernel_matrix, y)
 
-kgcv.best_score_, kgcv.best_params_
+print(kgcv.best_score_, kgcv.best_params_)
 
 plot_performance(kgcv)
 
-refit_kgcv = (kssvm, param_grid, score_survival_model,
-                    n_jobs=4, iid=False, refit=True,
-                    cv=cv)
+refit_kgcv = GridSearchCV(kssvm, param_grid, score_survival_model,
+                          n_jobs=4, iid=False, refit=True,
+                          cv=cv)
 refit_kgcv = refit_kgcv.fit(kernel_matrix, y)
+fast_kernel_SSVM_gcvrank = refit_kgcv.predict(kernel_matrix)
 
-##def fit_and_score_features(X, y):
-##    n_features = X.shape[1]
-##    scores = np.empty(n_features)
-##    m = CoxPHSurvivalAnalysis()
-##    for j in range(n_features):
-##        Xj = X[:, j:j+1]
-##        m.fit(Xj, y)
-##        scores[j] = m.score(Xj, y)
-##    return scores
-##
-##scores = fit_and_score_features(X.values, y)
-##all_predictors=pd.Series(scores, index=X.columns).sort_values(ascending=False)
-##
-#from sklearn.feature_selection import SelectKBest
-#from sklearn.pipeline import Pipeline
-#
-#pipe = Pipeline([('encode', OneHotEncoder()),
-#                 ('select', SelectKBest(fit_and_score_features, k=3)),
-#                 ('model', CoxPHSurvivalAnalysis())])
-#
-#param_grid = {'select__k': np.arange(1, X.shape[1] + 1)}
-#gcv = GridSearchCV(pipe, param_grid, return_train_score=True)
-#gcv.fit(X, y)
-#
-#pd.DataFrame(gcv.cv_results_).sort_values(by='mean_test_score', ascending=False)
-#pipe.set_params(**gcv.best_params_)
-#pipe.fit(X, y)
-#
-#encoder, transformer, final_estimator = [s[1] for s in pipe.steps]
-#best_predictors=pd.Series(final_estimator.coef_, index=encoder.encoded_columns_[transformer.get_support()])
 
-pred_curves = estimator.predict_survival_function(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                   y,test_size=0.2,
+                                                   random_state=4)
+estimator = CoxPHSurvivalAnalysis(alpha=9.5367431640625e-07)
+estimator.fit(X_train, y_train)
+
+pred_curves = estimator.predict_survival_function(X_test)
 for curve in pred_curves:
     plt.step(curve.x, curve.y, where="post")
+    
+plt.clf()
 
-Xt = OneHotEncoder().fit_transform(X)
+cum_curv = estimator.predict_cumulative_hazard_function(X_test)
+for item in cum_curv:
+    plt.step(item.x, item.y, where="post")
+plt.clf()
 
-cv = KFold(n_splits=5, shuffle=True, random_state=312188)
-coxnet = CoxnetSurvivalAnalysis(n_alphas=60,
-                                l1_ratio=0.2, alpha_min_ratio=0.01,
-                                verbose=True).fit(X, y)
-gcv = GridSearchCV(coxnet,
-                   {"alphas": [[v] for v in coxnet.alphas_]},
-                   cv=cv).fit(X, y)
-
-scores = gcv.cv_results_['mean_test_score']
-scores_std = gcv.cv_results_['std_test_score']
